b4ae9b7d9227830ce4d98378cbf1dcd28f3d9ba0
BronwenM <bronwen.marchant@gmail.com>
2024-11-01 21:04:48 -0700

    Added button functionality in modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import { ShowModalContext } from "App";
 
 const PhotoDetailsModal = () => {
+  const setShowModal = useContext(ShowModalContext);
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={() => setShowModal(prev => !prev)}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
