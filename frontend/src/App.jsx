534d7b2e8f74bdc94df1b4dfbc73955116482825
BronwenM <bronwen.marchant@gmail.com>
2024-11-04 12:37:15 -0800

    Used backend to make db calls to populate photos and topics data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
-import { createContext } from "react";
+import { useEffect, createContext } from "react";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import useApplicationData from "hooks/useApplicationData";
-import photos from "mocks/photos";
-import topics from "mocks/topics";
+// import photos from "mocks/photos";
+// import topics from "mocks/topics";
 export const FavouritePhotosContext = createContext();
 export const ShowModalContext = createContext();
 
 const App = () => {
-  const {favouritePhotos, showModal, modalData, toggleFavourite, toggleModal, loadModalData} = useApplicationData(photos, topics);
+  const {photoData, topicData, favouritePhotos, showModal, modalData, toggleFavourite, toggleModal, loadModalData, loadPhotosData, loadTopicData} = useApplicationData();
+  
+  useEffect(() => {
+    fetch('/api/photos')
+    .then(res => res.json())
+    .then(data => loadPhotosData(data));
+    
+    fetch('/api/topics')
+    .then(res => res.json())
+    .then(data => loadTopicData(data));
+    
+  }, [])
+  
     
   return (
     <FavouritePhotosContext.Provider value={{favouritePhotos, toggleFavourite}}>
       <ShowModalContext.Provider value={{toggleModal, loadModalData, modalData}}>
         <div className="App">
-          <HomeRoute photos={photos} topics={topics} />
+          <HomeRoute photos={photoData} topics={topicData} />
           {showModal && <PhotoDetailsModal />}
         </div>
       </ShowModalContext.Provider>
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,15 +1,19 @@
-import { useState, useReducer } from "react"
+import { useReducer } from "react"
 
 export const ACTIONS = {
   TOGGLE_FAV_PHOTO: 'TOGGLE_FAV_PHOTO',
   TOGGLE_MODAL_VIEW: 'TOGGLE_MODAL_VIEW',
-  SET_MODAL_DATA: 'SET_MODAL_DATA'
+  SET_MODAL_DATA: 'SET_MODAL_DATA',
+  LOAD_PHOTO_DATA: 'LOAD_PHOTO_DATA',
+  LOAD_TOPIC_DATA:'LOAD_TOPIC_DATA',
 }
 
 const initialState = {
   favouritePhotos: [],
   showModal: false,
-  modalData: {}
+  modalData: {},
+  photoData: [],
+  topicData: []
 }
 
 const reducer = (state, action) => {
@@ -31,28 +35,47 @@ const reducer = (state, action) => {
         modalData: { ...action.photoObj }
       }
 
+    case 'LOAD_PHOTO_DATA':
+      return {
+        ...state,
+        photoData: [...action.data]
+      }
+
+    case 'LOAD_TOPIC_DATA':
+      return {
+        ...state,
+        topicData: [...action.data]
+      }
+
     default:
       throw new Error(`ERROR! BAD ACTION ${action.type}`);
   }
 }
 
-const useApplicationData = (photos, topics) => {
+const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFavourite = (photoID) => {
-    dispatch({type: 'TOGGLE_FAV_PHOTO', photoID, photos})
-    console.log('state.favouritePhotos', state.favouritePhotos)
+    dispatch({type: 'TOGGLE_FAV_PHOTO', photoID, photos: state.photoData});
   }
 
   const toggleModal = () => {
-    dispatch({type: 'TOGGLE_MODAL_VIEW'})
+    dispatch({type: 'TOGGLE_MODAL_VIEW'});
   }
 
   const loadModalData = (photoObj) => {
-    dispatch({type: 'SET_MODAL_DATA', photoObj})
+    dispatch({type: 'SET_MODAL_DATA', photoObj});
   }
 
-  return { favouritePhotos: state.favouritePhotos, showModal: state.showModal, modalData: state.modalData, toggleFavourite, toggleModal, loadModalData }
+  const loadPhotosData = (data) => {
+    dispatch({type: 'LOAD_PHOTO_DATA', data});
+  }
+
+  const loadTopicData = (data) => {
+    dispatch({type: 'LOAD_TOPIC_DATA', data})
+  }
+
+  return { photoData: state.photoData, topicData: state.topicData, favouritePhotos: state.favouritePhotos, showModal: state.showModal, modalData: state.modalData, toggleFavourite, toggleModal, loadModalData, loadPhotosData, loadTopicData }
 }
 
 export default useApplicationData;
